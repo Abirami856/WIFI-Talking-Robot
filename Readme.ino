f7cb088cef4a7389b4d4f722e6a51783f881fe07
Abirami856 <abiramib493@gmail.com>
2026-06-02 13:36:07 +0530

    Update Readme.ino

--- Readme.ino
+++ Readme.ino
@@ -1,4 +1,5 @@
-
+The Wi-Fi Talking Robot is an intelligent robotic system that can communicate with users through voice messages and be controlled over a Wi-Fi network. The robot receives commands wirelessly from a smartphone, computer, or web application and responds by performing actions such as moving, speaking predefined messages, or providing status information.
+The system typically uses a Wi-Fi-enabled microcontroller such as the ESP8266 , along with motors, a speaker, and other electronic components. Through wireless communication, users can remotely control the robot and interact with it in real time
 
 
 
